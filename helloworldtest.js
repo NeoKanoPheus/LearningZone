543bc0594c8aeb8aadc359e75a4c749a5c38d4aa
NeoKanoPheus <68460458+NeoKanoPheus@users.noreply.github.com>
2020-09-17 04:47:40 -0700

    Delete helloworldtest.js

--- helloworldtest.js
+++
@@ -1,8 +0,0 @@
-const Helloworld = artifacts.require("Helloworld");
-contract ("HelloWorld", async function() {
-  it("should initialize correctly", async function() {
-    let instance = await Helloworld.deployed();
-    let message = await instance.getMessage();
-    assert(message ==="Helloooo Againnnn Eboooo...");
-  });
-});
